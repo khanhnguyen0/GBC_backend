c8a813ed147dc72f58be072f00ae041d911c7f48
Khanh Nguyen <khanh@rnd.works>
2017-09-15 16:26:43 +0300

    init

---
+++ index.js
@@ -0,0 +1,215 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+
+var PORT = process.env.PORT || 5000
+
+const Application = require('./models/application.js');
+const Supplier = require('./models/supplier.js');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://root:root@ds133241.mlab.com:33241/neste')
+
+const corsOption = {
+    origin: "*",
+    credentials: true
+}
+
+function randomDate(start, end) {
+  var date = new Date(+start + Math.random() * (end - start));
+  return date;
+}
+
+app.use(bodyparser.json())
+app.use('/', cors(corsOption))
+
+app.get('/application/list',(req,res)=>{
+  Application.find({},(err,a)=>{
+    if (err) return res.status(500).send('Database error');
+    return res.json(a);
+  })
+})
+
+
+app.get('/supplier/list',(req,res)=>{
+  Supplier.find({},(err,a)=>{
+    if (err) return res.status(500).send('Database error');
+    return res.json(a);
+  })
+})
+
+
+
+
+app.post('/application/new',(req,res)=>{
+  console.log(req.body);
+  var a = new Application({
+    "code": req.body.application.code,
+    "supplier_name": req.body.application.supplier_name,
+    "feedstock_type": req.body.application.feedstock_type,
+    "company_name": req.body.application.company_name,
+    "country": req.body.application.country,
+    "province": req.body.application.province,
+    "current_status": req.body.application.current_status,
+    "current_team": req.body.application.current_team,
+    "processed_team": req.body.application.processed_team,
+    "latest_update": req.body.application.latest_update,
+    "risk": req.body.application.risk,
+    "doc_fulfilled": req.body.application.doc_fulfilled,
+    "cert_no": req.body.application.cert_no,
+    "attachment": req.body.application.attachment,
+    submission_date: "01.01.2017"
+  });
+
+  a.save((err,a)=>{
+    if (err) return res.status(500).send('Database Error');
+
+    return res.json({id:a._id});
+  })
+});
+
+
+
+app.post('/supplier/new',(req,res)=>{
+  console.log(req.body);
+  var a = new Supplier({
+    "code": req.body.supplier.code,
+    "supplier_name": req.body.supplier.supplier_name,
+    "feedstock_type": req.body.supplier.feedstock_type,
+    "company_name": req.body.supplier.company_name,
+    "country": req.body.supplier.country,
+    "province": req.body.supplier.province,
+    "contract_date": req.body.supplier.contract_date,
+    "certification_no": req.body.supplier.certification_no,
+    "certification_name": req.body.supplier.certification_name,
+    "capacity": req.body.supplier.capacity,
+    "volume": req.body.supplier.volume,
+    "no_estate": req.body.supplier.no_estate,
+    "sustainability_commitment": req.body.supplier.sustainability_commitment,
+    "traceability_logistics": req.body.supplier.traceability_logistics,
+    "stakeholder_engagement": req.body.supplier.stakeholder_engagement,
+    "social_resp": req.body.supplier.social_resp,
+    "environment": req.body.supplier.environment,
+    "GHG": req.body.supplier.GHG,
+    "expired_certification_no": req.body.supplier.expired_certification_no,
+    "rank": req.body.supplier.rank,
+    "quality_rate": req.body.supplier.quality_rate,
+    "traceability": req.body.supplier.traceability,
+    "ontime_delivery": req.body.supplier.ontime_delivery,
+    "updated_document": req.body.supplier.updated_document,
+    "current_GHG": req.body.supplier.current_GHG,
+    sus_rating:Math.floor(Math.random() * 100),
+    fin_rating:Math.floor(Math.random() * 100),
+    sec_rating:Math.floor(Math.random() * 100),
+    sup_rating:Math.floor(Math.random() * 100),
+    bio_rating:Math.floor(Math.random() * 100),
+    start_date:randomDate(1262378795,1420145195),
+    end_date:randomDate(1514839595,1577911595),
+    position: {
+      lat:Math.random()*90,
+      lng:Math.random()*180
+    }
+  });
+
+  a.save((err,a)=>{
+    if (err) return res.status(500).send('Database Error');
+    return res.json({id:a._id});
+  })
+});
+
+app.post('/supplier/bulk',(req,res)=>{
+  var suppliers = req.body.supplier.map(s=>{
+    return new Supplier({
+      "code": s.code,
+      "supplier_name": s.supplier_name,
+      "feedstock_type": s.feedstock_type,
+      "company_name": s.company_name,
+      "country": s.country,
+      "province": s.province,
+      "contract_date": s.contract_date,
+      "certification_no": s.certification_no,
+      "certification_name": s.certification_name,
+      "capacity": s.capacity,
+      "volume": s.volume,
+      "no_estate": s.no_estate,
+      "sustainability_commitment": s.sustainability_commitment,
+      "traceability_logistics": s.traceability_logistics,
+      "stakeholder_engagement": s.stakeholder_engagement,
+      "social_resp": s.social_resp,
+      "environment": s.environment,
+      "GHG": s.GHG,
+      "expired_certification_no": s.expired_certification_no,
+      "rank": s.rank,
+      "quality_rate": s.quality_rate,
+      "traceability": s.traceability,
+      "ontime_delivery": s.ontime_delivery,
+      "updated_document": s.updated_document,
+      "current_GHG": s.current_GHG,
+      sus_rating:Math.floor(Math.random() * 100),
+      fin_rating:Math.floor(Math.random() * 100),
+      sec_rating:Math.floor(Math.random() * 100),
+      sup_rating:Math.floor(Math.random() * 100),
+      bio_rating:Math.floor(Math.random() * 100),
+      start_date:randomDate(1262378795000,1420145195000),
+      end_date:randomDate(1514839595000,1577911595000),
+      position: {
+        lat:Math.random()*90,
+        lng:Math.random()*180
+      }
+    })
+  });
+  console.log(suppliers);
+  Supplier.insertMany(suppliers)
+  .then(a=>{
+    console.log(a);
+    return res.status(200).send('done');
+  })
+  .catch(err=>{
+    console.log(err);
+    return res.status(500).send('Database error');
+  })
+})
+
+
+
+
+app.post('/application/bulk',(req,res)=>{
+  var applications = req.body.application.map(a=>{
+    return new Application({
+      "code": a.code,
+      "supplier_name": a.supplier_name,
+      "feedstock_type": a.feedstock_type,
+      "company_name": a.company_name,
+      "country": a.country,
+      "province": a.province,
+      "current_status": a.current_status,
+      "current_team": a.current_team,
+      "processed_team": a.processed_team,
+      "latest_update": a.latest_update,
+      "risk": a.risk,
+      "doc_fulfilled": a.doc_fulfilled,
+      "cert_no": a.cert_no,
+      "attachment": a.attachment,
+      submission_date: "01.01.2017"
+    })
+  });
+  console.log(applications);
+  Application.insertMany(applications)
+  .then(a=>{
+    console.log(a);
+    return res.status(200).send('done');
+  })
+  .catch(err=>{
+    console.log(err);
+    return res.status(500).send('Database error');
+  })
+})
+
+
+
+server.listen(PORT, () => {
+    console.log('Running on port', PORT)
+})
---
+++ models/supplier.js
@@ -0,0 +1,43 @@
+const mongoose  = require('mongoose');
+
+const supplierSchema = mongoose.Schema({
+   "code": {type:String},
+   "supplier_name": {type:String},
+   "feedstock_type": {type:String},
+   "company_name": {type:String},
+   "country": {type:String},
+   "province": {type:String},
+   "contract_date": {type:String},
+   "certification_no": {type:Number},
+   "certification_name": {type:String},
+   "capacity": {type:Number},
+   "volume": {type:Number},
+   "no_estate": {type:Number},
+   "sustainability_commitment": {type:Number},
+   "traceability_logistics": {type:Number},
+   "stakeholder_engagement": {type:Number},
+   "social_resp": {type:Number},
+   "environment": {type:Number},
+   "GHG": {type:Number},
+   "expired_certification_no": {type:Number},
+   "rank": {type:Number},
+   "quality_rate": {type:Number},
+   "Traceability": {type:Number},
+   "ontime_delivery": {type:Number},
+   "updated_document": {type:Number},
+   "current_GHG": {type:Number},
+   sus_rating:{type:Number},
+   fin_rating:{type:Number},
+   sec_rating:{type:Number},
+   sup_rating:{type:Number},
+   bio_rating:{type:Number},
+   start_date:{type:Date},
+   end_date:{type:Date},
+   position: {
+     lat:{type:Number},
+     lng:{type:Number}
+   }
+})
+
+
+module.exports  = mongoose.model('Supplier', supplierSchema);
